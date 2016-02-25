b604a09048a5778a0e3511c28c953da0f80f5f99
CarexZ <innazu@gmail.com>
2016-02-25 22:40:12 +0300

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,19 +3,19 @@
 
 
 ## This function does nothing but save the matrix and creates a list of functions applicable
-## to this particular matrix and also creates cache variable s to store the solve result. 
+## to this particular matrix and also creates cache variable s to store the solve function result. 
 ## In the beginning s set to NULL, and after matrix solved, the result will be stored here.
 
 makeCacheMatrix <- function(x = matrix()) {
-        s<-NULL
-        setMatrix<- function (y) {
+        s<-NULL ##initially set 's' to NULL
+        set<- function (y) { ##this part is actually useless and can be omitted, as it will be never called by cacheSolve
                 x<<-y
                 s<<-NULL
         }
-        getMatrix <-function() x
-        setSolve <-function(solve) s<<-solve
-        getSolve <- function () s
-        list (set=setMatrix, get=getMatrix, setSolve=setSolve, getSolve=getSolve)
+        get <-function() x ##return the matrix
+        setSolve <-function(solve) s<<-solve ##assign 'solve' to s 'variable'
+        getSolve <- function() s ##return 's' (solved matrix or NULL)
+        list (set=set, get=get, setSolve=setSolve, getSolve=getSolve)
 }
 
 
@@ -26,13 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         s<-x$getSolve()
-        if (!is.null(s)){
+        if (!is.null(s)) { ##check if this matrix was solved before
                 message("getting cached data")
-                return(s)
+                return(s) ##return result from cache and exit the function
         }
-        data<-x$get()
-        s<-solve(data, ...)
-        x$setSolve(s)
+        data<-x$get()  ##if matrix was not solved, assign matrix 'x' to 'data' variable
+        s<-solve(data, ...) ##solve this matrix
+        x$setSolve(s)  ##save the result to s
         s  ## Return a matrix that is the inverse of 'x'
          
 }
